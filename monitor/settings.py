1ab6a6545eff438f08e1f1984e9e2cceed00dd0e
vinodh kumar <vinodh17k@gmail.com>
2019-12-29 03:36:01 +0530

    mail test

--- monitor/settings.py
+++ monitor/settings.py
@@ -114,9 +114,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 SENDGRID_API_KEY = os.getenv('SENDGRID_API_KEY')
 EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'vinodh17k'
+EMAIL_HOST_USER = 'apikey'
 EMAIL_HOST_PASSWORD = SENDGRID_API_KEY
-EMAIL_USE_TLS = True
+# EMAIL_USE_TLS = True
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
--- monitor/views.py
+++ monitor/views.py
@@ -37,7 +37,7 @@ def get_cpu_utilization(request):
         return Response({'cpu_percent':cpu_percent, 'ram_percent':ram_percent})
 
 def sendmail(request):
-    check = send_mail('Subject here', 'Here is the message.', 'from@example.com', ['vinodh17k@gmail.com'], fail_silently=False)
+    check = send_mail('Subject here', 'Here is the message.', 'vinodh17k@gmail.com', ['vinodh17k@gmail.com'], fail_silently=False)
     if check :
         return HttpResponse('done')
     else:
