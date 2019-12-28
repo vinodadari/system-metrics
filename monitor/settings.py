850fe7cda8d110fb3be071c98bfaec1519214510
vinodh kumar <vinodh17k@gmail.com>
2019-12-29 03:32:28 +0530

    mail test

--- monitor/settings.py
+++ monitor/settings.py
@@ -114,7 +114,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 SENDGRID_API_KEY = os.getenv('SENDGRID_API_KEY')
 EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'apikey'
+EMAIL_HOST_USER = 'vinodh17k'
 EMAIL_HOST_PASSWORD = SENDGRID_API_KEY
 EMAIL_USE_TLS = True
 
